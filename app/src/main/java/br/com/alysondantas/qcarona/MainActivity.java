29d58a43c79f9324a3e5d70c05091e330937fd71
Alyson Dantas <alyson.lipe@gmail.com>
2017-11-30 23:30:25 -0300

    Initial commit

---
+++ app/src/main/java/br/com/alysondantas/qcarona/MainActivity.java
@@ -0,0 +1,28 @@
+package br.com.alysondantas.qcarona;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button cadastrarbutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cadastrarbutton = (Button) findViewById(R.id.cadastrarbutton);
+
+        cadastrarbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,CadastrarActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
