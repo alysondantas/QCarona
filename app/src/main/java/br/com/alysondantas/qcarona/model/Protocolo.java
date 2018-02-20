210e162742a9fa33620766de239e4f8431b0bc48
marcosviniciuscl <marcosviniciuscl@outlook.com>
2018-02-20 20:44:06 -0300

    Correçoes

--- app/src/main/java/br/com/alysondantas/qcarona/model/Protocolo.java
+++ app/src/main/java/br/com/alysondantas/qcarona/model/Protocolo.java
@@ -16,6 +16,8 @@ public abstract class Protocolo {
         public static final int OBTEM_PERFIL = 6;
         public static final int BUSCAR_AMIGOS = 7;
         public static final int DESFAZ_AMIGO = 8;
+        public static final int BUSCA_SOLICITACAO_AMIZADE = 9;
+        public static final int CIDADES_DISPONIVEIS = 10;
     }
 
     public abstract class Notificacao {
@@ -28,5 +30,7 @@ public abstract class Protocolo {
         public static final int JA_EXISTE_SOLICITACAO_AMIZ = 106;
         public static final int CARONA_DISPONIVEL = 107;
         public static final int OPERACAO_CONCLUIDA = 109;
+        public static final int OPERACAO_NAO_CONCLUIDA = 110;
+        public static final int RETORNO_CIDADES_DISPONIVEIS = 111;
     }
 }
